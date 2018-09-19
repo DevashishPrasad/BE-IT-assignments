c6772a9b6609424e46f8d0c820da9106b7dcff7d
Devashish Prasad <devashishkprasad@gmail.com>
2018-09-19 07:34:35 +0530

    Update files.cpp

--- SE/1st sem/OOPL/files.cpp
+++ SE/1st sem/OOPL/files.cpp
@@ -1,20 +1,89 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
 
 using namespace std;
+
+class Student
+{
+    char *name;
+    int rollno;
+    int marks;
+
+    public:
+    friend void input(Student s[], int n);
+    friend void read( int n);
+};
+
+void input(Student s[], int n)
+{
+    int i;
+
+    for(i = 0; i < n; i++)
+    {
+        cout<<"\n\nRecord number "<<i+1<<endl;
+        s[i].name = new char[20];
+        cout<<"Enter name - "<<endl;
+        cin>>s[i].name;
+        cout<<"Enter rollno - "<<endl;
+        cin>>s[i].rollno;
+        cout<<"Enter marks - "<<endl;
+        cin>>s[i].marks;
+    }
+}
+
+void write(Student s[], int n)
+{
+    fstream f;
+    f.open("abc.txt",ios::out);
+    int i;
+
+    for(i = 0; i < n; i++)
+    {
+        f.write((char *) &s[i], sizeof(s[i]));
+    }
+    f.close();
+    cout<<"\n\nRecords written in file successfully"<<endl;
+}
+
+void read(int n)
+{
+    cout<<"\n\nReading records from file"<<endl;
+    fstream f;
+    f.open("abc.txt",ios::in);
+    int i;
+    
+    Student temp;
+    
+    cout<<"NAME\tROLLNO\tMARKS"<<endl;
+    for(i = 0; i < n; i++)
+    {
+        f.read((char *) &temp, sizeof(temp));
+        cout<<i+1<<temp.name<<"\t"<<temp.rollno<<"\t"<<temp.marks<<endl;
+    }
+    
+    f.close();
+}
+
 int main()
 {
-    //char ch;
-    string line;
-    fstream obj("abc.txt",ios::in);
-//    while(obj>>ch)
-  //   cout<<ch;
-    while(getline(obj, line))
-    {
-          cout<<line<<endl;
-    }
-    obj.close();
+    int n=0;
+    Student *s;
     
-    return 1;
+    while(n == 0)
+    {
+        cout<<"Enter the number of records to be writen - "<<endl;
+        cin>>n;
+        if(n>0)
+        {
+            s = new Student[n];
+            break;
+        }
+        cout<<"Invalid value...  Please try again"<<endl;
+    }
+    
+    input(s, n);
+    write(s, n);
+    read(n);
+
+    return EXIT_SUCCESS;
 }
