1855f6e23b3a9df873def75857c693a8f893cb54
Devashish Prasad <devashishkprasad@gmail.com>
2018-09-19 13:17:37 +0530

    not completed

--- SE/1st sem/OOPL/seqfiles.cpp
+++ SE/1st sem/OOPL/seqfiles.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-class Database{
+class Student
+{
 
     char *name;
     char *phone;
@@ -12,117 +13,133 @@ class Database{
     long int salary;
     
     public:
+    Student()
+    {
+         name = new char[20];
+         phone= new char[20];
+    }
     
-    friend void createdb(struct database record[100], int n,fstream *fp);
-    friend void display(struct database record[100], fstream *fp);
-    friend void add(struct database record[100], int n, fstream *fp);
+    friend void createdb(Student record[100], int n);
+    friend void display(Student record[100]);
+    friend void add(Student record[100], int n, fstream *fp);
     friend void search(int id, fstream *fp);
     friend void modify(int id, fstream *fp);
-    friend void delete(int id, fstream *fp);
+    friend void  del(int id, fstream *fp);
 };
 
-void createdb(struct database record[100], int n,fstream *fp)
+void createdb(Student record[100], int n)
 {
-	fp = fopen("database.txt","w+");
-	struct database student;
+    fstream fp;
+    Student student;
 
     int i,j,flg=0;
     for(i=0; i<n; i++)
     {
-        printf("\n\nRecord Number - %d ",i+1);
-        printf("\nEnter the id of the employee - ");
+        fp.open("database.txt", ios::in);
+        cout<<"\n\nRecord Number - "<<i+1;
+        cout<<"\nEnter the id of the employee - ";
         while(1)
         {
             flg = 0;
-            scanf ("%d", &record[i].id);
-            while(fread(&student, sizeof(struct database),1, fp))
-                if(student.id == record[i].id)
+            cin>>record[i].id;
+            while(fp.read((char *) &student, sizeof(student)))
+            {  cout<<"kjkjkj";
+               if(student.id == record[i].id)
                 {
                     flg = 1;
-                    printf("\nID already exist, please enter another ID -");
-					break;
+                    cout<<"\nID already exist, please enter another ID -";
+                    break;
                 }
+            }
             if(flg == 0) break;
 
         }
-        printf("\nEnter the name of the employee - ");
-        scanf ("%s", &record[i].name);
-        printf("\nEnter the phone number of the employee - ");
+        fp.close();
+        cout<<"\nEnter the name of the employee - ";
+        cin>>record[i].name;
+        cout<<"\nEnter the phone number of the employee - ";
+        
         while(1)
         {
-            scanf ("%s", &record[i].phone);
+            cin>>record[i].phone;
             if(strlen(record[i].phone) == 10) break;
-            else printf("\nInvalid phone number, Please try again - ");
+            else cout<<"\nInvalid phone number, Please try again - ";
         }
-        printf("\nEnter the salary of the employee - ");
-        scanf ("%ld", &record[i].salary);
-		fwrite(&record[i], sizeof(struct database), 1, fp);
+        
+        cout<<"\nEnter the salary of the employee - ";
+        cin>>record[i].salary;
+        
+        fp.open("database.txt", ios::out);
+        if(fp.write((char *) &record[i], sizeof(&record[i]))) cout<<"TRUE";
+        fp.close();
     }
-	fclose(fp);
 }
-void display(struct database record[100], fstream *fp)
+void display(Student record[100])
 {
-    char ch;
-    struct database student;
+    fstream fp;
+    Student student;
 
-	fp = fopen("database.txt","r");
+    fp.open("database.txt",ios::in);
 
-	if(fp == NULL) return;
-	else
-	{
-		printf("\nID\tPHONE     \tSALARY\t\tNAME");
-		while(fread(&student, sizeof(struct database),1, fp))
-		{
-			printf("\n%d\t%s\t%ld\t\t%s", student.id, student.phone, student.salary, student.name);
-		}
-	}
-	fclose(fp);
+    if(fp == NULL) return;
+    else
+    {
+         cout<<"\nID\tPHONE     \tSALARY\t\tNAME";
+         while()
+         {
+             fp.read((char *) &student, sizeof(student));
+             cout<<"sdsdsd";
+             cout<<student.id<<"\t"<< student.phone<<"\t"<<student.salary<<"\t"<<student.name;
+         }
+    }
+    fp.close();
 }
-void add(struct database record[100], int n, fstream *fp)
+
+void add(Student record[100], int n, fstream *fp)
 {
-	struct database student;
-    fp = fopen("database.txt","a+");
+    Student student;
+    fp->open("database.txt",ios::app);
     int i,flg=0;
     for(i=0; i<n; i++)
     {
-        printf("\n\nRecord Number - %d ",i+1);
-        printf("\nEnter the id of the employee - ");
+        cout<<"\n\nRecord Number - "<<i+1;
+        cout<<"\nEnter the id of the employee - ";
         while(1)
         {
             flg = 0;
-            scanf ("%d", &record[i].id);
-            while(fread(&student, sizeof(struct database),1, fp))
+            cin>>record[i].id;
+            while(fp->read((char *) &student, sizeof(student)))
                 if(student.id == record[i].id)
                 {
                     flg = 1;
-                    printf("\nID already exist, please enter another ID -");
-					break;
+                    cout<<"\nID already exist, please enter another ID -";
+                    break;
                 }
             if(flg == 0) break;
         }
-        printf("\nEnter the name of the employee - ");
-        scanf ("%s", &record[i].name);
-        printf("\nEnter the phone number of the employee - ");
+        cout<<"\nEnter the name of the employee - ";
+        cin>>record[i].name;
+        cout<<"\nEnter the phone number of the employee - ";
         while(1)
         {
-            scanf ("%s", &record[i].phone);
+            cin>>record[i].phone;
             if(strlen(record[i].phone) == 10) break;
-            else printf("\nInvalid phone number, Please try again - ");
+            else cout<<"\nInvalid phone number, Please try again - ";
         }
-        printf("\nEnter the salary of the employee - ");
-        scanf ("%ld", &record[i].salary);
-		fwrite(&record[i], sizeof(struct database), 1, fp);
+        cout<<"\nEnter the salary of the employee - ";
+        cin>>record[i].salary;
+        fp->write((char *) &record[i], sizeof(student));
     }
-	fclose(fp);
+    fp->close();
 }
 void search(int id, fstream *fp)
 {
-	struct database student;
-    fp = fopen("database.txt","r");
+    Student student;
+    fp->open("database.txt",ios::in);
 
     int i,val=-1;
 
-    while(fread(&student, sizeof(struct database),1, fp))
+    while(fp->read((char *) &student, sizeof(student)))
         if(student.id == id)
         {
             val = 1;
@@ -130,132 +147,134 @@ void search(int id, fstream *fp)
         }
 
     if(val == -1)
-        printf("\nNo record found...");
+        cout<<"\nNo record found...";
     else
     {
-        printf("\nRecord Found");
-        printf("\nID\tPHONE     \tSALARY\t\tNAME");
-        printf("\n%d\t%s\t%ld",student.id, student.phone, student.salary);
-        printf("\t\t");
+        cout<<"\nRecord Found";
+        cout<<"\nID\tPHONE     \tSALARY\t\tNAME";
+        cout<<"\n"<<student.id<<"\t"<< student.phone<<"\t"<< student.salary;
+        cout<<"\t\t";
         puts(student.name);
     }
 
-	fclose(fp);
+    fp->close();
 }
 void modify(int id, fstream *fp)
 {
-	struct database student,s;
-    fp = fopen("database.txt","r");
-	fstream np = fopen("temp.txt","w");
+    Student student,s;
+    fp->open("database.txt",ios::in);
+    fstream np; 
+    np.open("temp.txt",ios::out);
 
     int i,val=-1;
 
-    while(fread(&student, sizeof(struct database),1, fp))
-	{   
-		if(student.id == id)
-		{
-			printf("\nEnter the name of the employee - ");
-			scanf ("%s", &student.name);
-			printf("\nEnter the phone number of the employee - ");
-			while(1)
-			{
-				scanf ("%s", &student.phone);
-				if(strlen(student.phone) == 10) break;
-				else printf("\nInvalid phone number, Please try again - ");
-			}
-			printf("\nEnter the salary of the employee - ");
-			scanf ("%ld", &student.salary);
-			fwrite(&student, sizeof(struct database), 1, np);
-			continue;
-		}
-		fwrite(&student, sizeof(struct database),1, np);
-	}
+    while(fp->read((char *) &student, sizeof(student)))
+    {   
+        if(student.id == id)
+        {
+            cout<<"\nEnter the name of the employee - ";
+            cin>>student.name;
+            cout<<"\nEnter the phone number of the employee - ";
+            while(1)
+            {
+                cin>>student.phone;
+                if(strlen(student.phone) == 10) break;
+                else cout<<"\nInvalid phone number, Please try again - ";
+            }
+            cout<<"\nEnter the salary of the employee - ";
+            cin>>student.salary;
+            np.write((char *) &student, sizeof(Student));
+            continue;
+        }
+        np.write((char *) &student, sizeof(student));
+    }
 
-	fclose(fp);
-	fclose(np);
-	remove("database.txt");
-	rename("temp.txt","database.txt");
+    fp->close();
+    np.close();
+    remove("database.txt");
+    rename("temp.txt","database.txt");
 }
-void delete(int id, fstream *fp)
+void del(int id, fstream *fp)
 {
-    struct database student,s;
-    fp = fopen("database.txt","r");
-	fstream np = fopen("temp.txt","w");
+    Student student,s;
+    fp->open("database.txt",ios::in);
+    fstream np;
+    np.open("temp.txt",ios::out);
 
     int i,val=-1;
 
-    while(fread(&student, sizeof(struct database),1, fp))
-	{   
-		if(student.id == id)
-		{
-			continue;
-		}
-		fwrite(&student, sizeof(struct database),1, np);
-	}
+    while(fp->read((char *) &student, sizeof(student)))
+    {   
+        if(student.id == id)
+        {
+            continue;
+        }
+        fp->write((char *) &student, sizeof(student));
+    }
 
-	fclose(fp);
-	fclose(np);
-	remove("database.txt");
-	rename("temp.txt","database.txt");
+    fp->close();
+    np.close();
+    remove("database.txt");
+    rename("temp.txt","database.txt");
 }
 
 int main(void) {
 
-	int ch,no=0,i;
-    Database records[100];
-	fstream fp;
+    int ch,no=0,i;
+    Student records[100];
+    fstream fp;
 
-	do{
-		printf("\n\n\n ******* MENU ******* ");
-		printf("\n 1. Create databse");
-		printf("\n 2. Display database");
-		printf("\n 3. Add record");
-		printf("\n 4. Search record");
-		printf("\n 5. Modify record");
-		printf("\n 6. Delete record");
-		printf("\n 7. exit");
-		printf("\n Enter your choice - ");
-		scanf("%d",&ch);
+    do{
+        cout<<"\n\n\n ******* MENU ******* ";
+        cout<<"\n 1. Create databse";
+        cout<<"\n 2. Display database";
+        cout<<"\n 3. Add record";
+        cout<<"\n 4. Search record";
+        cout<<"\n 5. Modify record";
+        cout<<"\n 6. Delete record";
+        cout<<"\n 7. exit";
+        cout<<"\n Enter your choice - ";
+        cin>>ch;
 
-		switch(ch)
-		{
-			case 1:
-                    printf("\nCreating new Database - ");
-					printf("\nEnter the number of Records to be entered - ");
-					scanf("%d",&no);
-                    createdb(records, no, &fp);
-					break;
-			case 2:    
-                    display(records, &fp);
-					break;
-			case 3:
-					printf("\nAdding records - ");
-					printf("\nEnter the number of Records to be entered - ");
-					scanf("%d",&no);
-					add(records, no, &fp);
-					break;
-			case 4:
-					printf("\nEnter the ID of the employee to be searched - ");
-					scanf("%d",&i);
-                    search(i, &fp);
-					break;
-			case 5:
-		            printf("\nEnter the ID of the employee to be modified - ");
-					scanf("%d",&i);
-                    modify(i, &fp);
-					break;
-			case 6:
-
-                    printf("\nEnter the ID of the employee to be deleted - ");
-					scanf("%d",&i);
-                    delete(i, &fp);
+        switch(ch)
+        {
+            case 1:
+                    cout<<"\nCreating new Database - ";
+                    cout<<"\nEnter the number of Records to be entered - ";
+                    cin>>no;
+                    createdb(records, no);
                     break;
-			case 7:
-				return 0;
-			default:
-				printf("\nPlease select a number form menu - ");
-				break;
-		}
-	}while(1);
-	return 0;
+            case 2:    
+                    display(records);
+                    break;
+            case 3:
+                    cout<<"\nAdding records - ";
+                    cout<<"\nEnter the number of Records to be entered - ";
+                    cin>>no;
+                    add(records, no, &fp);
+                    break;
+            case 4:
+                    cout<<"\nEnter the ID of the employee to be searched - ";
+                    cin>>i;
+                    search(i, &fp);
+                    break;
+            case 5:
+                    cout<<"\nEnter the ID of the employee to be modified - ";
+                    cin>>i;
+                    modify(i, &fp);
+                    break;
+            case 6:
+
+                    cout<<"\nEnter the ID of the employee to be  deld - ";
+                    cin>>i;
+                    del(i, &fp);
+                    break;
+            case 7:
+                    return 0;
+            default:
+                    cout<<"\nPlease select a number form menu - ";
+                    break;
+        }
+    }while(1);
+    return 0;
 }
