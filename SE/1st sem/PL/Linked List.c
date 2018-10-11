cee36dabc2103563b0dd2b34947b6cc9bbf0e815
Devashish Prasad <devashishkprasad@gmail.com>
2018-10-11 15:59:40 +0530

    Finally done with link list

--- SE/1st sem/PL/Linked List.c
+++ SE/1st sem/PL/Linked List.c
@@ -34,9 +34,87 @@ void display(struct Node *h)
 		printf(" %d -> ", h->data);
 		h = h->next;
 	}
+	printf(" NULL ");
 }
 
-struct Node * reverse (struct Node * head)
+struct Node * insert_front(struct Node* head)
+{
+	struct Node *t;
+	t = getNode();
+	t->next = head;
+	head = t;
+	return head;
+}
+
+struct Node * insert_middle(struct Node* head, int pos)
+{
+	struct Node *t, *p;
+	int i;
+
+	t = getNode();
+	p = head;
+	for(i = 0; i < pos-2 && p->next != NULL ; i++)
+		p = p->next;
+	t->next = p->next;
+	p->next = t;
+
+	return head;
+}
+
+struct Node * insert_last(struct Node* head)
+{
+	struct Node *t, *p;
+	t = getNode();
+	p = head;
+	while(p->next != NULL)
+		p = p->next;
+	p->next = t;
+
+	return head;
+}
+
+struct Node * delete_front(struct Node* head)
+{
+	struct Node *t;
+	t = head;
+	head = head->next;
+	free(t);
+	return head;
+}
+
+struct Node * delete_middle(struct Node* head, int pos)
+{
+	struct Node *t, *p;
+	int i;
+	p = head;
+	t = head;
+	for(i = 0; i < pos-1 && p->next != NULL; i++)
+	{
+		t = p;
+		p = p->next;
+	}
+	t->next = p->next;
+	free(p);
+
+	return head;
+}
+
+struct Node * delete_last(struct Node* head)
+{
+	struct Node *t, *p;
+	p = head;
+	t = head;
+	while(p->next != NULL)
+	{
+		t = p;
+		p = p->next;
+	}
+	t->next = p->next;
+	free(p);
+	return head;
+}
+
+struct Node * revert (struct Node * head)
 {
 	struct Node *c, *t, *r;
 	t = NULL;
@@ -45,10 +123,9 @@ struct Node * reverse (struct Node * head)
 
 	if(head->next == NULL)
 		return head;
-	
+
 	while(c != NULL)
 	{
-		printf(" ** ");
 		t = c->next;
 		c->next = r;
 		r = c;
@@ -57,11 +134,21 @@ struct Node * reverse (struct Node * head)
 
 	head = r;
 	return head;
-} 
+}
+
+void reverse (struct Node * head)
+{
+	if(head != NULL)
+	{
+		reverse(head->next);
+		printf(" %d ", head->data);
+	}
+}
+
 int main(void) {
 
 	int i, n, ch, pos;
-	struct Node *p, *t, *head=NULL;
+	struct Node *p, *head=NULL;
 
 	printf("\n Creating the Head -");
 
@@ -77,7 +164,9 @@ int main(void) {
 		p->next = getNode();
 		p = p->next;
 	}
-	do{
+
+	do
+	{
 		printf("\n\n\n MENU - ");
 		printf("\n 1. Insert at front");
 		printf("\n 2. Insert in middle");
@@ -85,9 +174,10 @@ int main(void) {
 		printf("\n 4. Delete from front");
 		printf("\n 5. Delete from middle");
 		printf("\n 6. Delete from last");
-		printf("\n 7. Reverse the list");
-		printf("\n 8. Display the list");
-		printf("\n 9. Exit");
+		printf("\n 7. Revert the list");
+		printf("\n 8. Reverse the list");
+		printf("\n 9. Display the list");
+		printf("\n 10. Exit");
 		printf("\n Enter your choice - ");
 		scanf("%d",&ch);
 
@@ -95,74 +185,49 @@ int main(void) {
 		{
 			case 1:
 					printf("\n\n Inserting at front - \n");
-					t = getNode();
-					t->next = head;
-					head = t;
+					head = insert_front(head);
 					break;
 			case 2:
 					printf("\n\n Inserting in middle - Enter the position - ");
 					scanf("%d", &pos);
-					t = getNode();			
-					p = head;		
-					for(i = 0; i < pos-2 && p->next != NULL ; i++)
-						p = p->next;
-					t->next = p->next;
-					p->next = t;
+					head = insert_middle(head, pos);
 					break;
 			case 3:
 					printf("\n\n Insertion at last - \n");
-					t = getNode();
-					p = head;
-					while(p->next != NULL)
-						p = p->next;
-					p->next = t;	
+					head = insert_last(head);
 					break;
 			case 4:
 					printf("\n\n Deletion at first - \n");
-					t = head;
-					head = head->next;
-					free(t);
+					head = delete_front(head);
 					printf("\n Deleted successfully");
 					break;
 			case 5:
 					printf("\n\n Deletion at middle - Enter the position \n");
 					scanf("%d", &pos);
-					p = head;
-					t = head;
-					for(i = 0; i < pos-1 && p->next != NULL; i++)
-					{
-						t = p;
-						p = p->next;
-					}	
-
-					t->next = p->next;
-					free(p);
+					head = delete_middle(head, pos);
 					printf("\n Deleted successfully");
 					break;
 			case 6:
 					printf("\n\n Deletion at end - \n");
-					p = head;
-					t = head;
-					while(p->next != NULL)
-					{
-						t = p;
-						p = p->next;
-					}	
-					t->next = p->next;
-					free(p);
+					head = delete_last(head);
 					printf("\n Deleted successfully");
 					break;
 			case 7:
-					printf("\n\n Reversing your list - ");
-					head = reverse(head);
+					printf("\n\n Reverting your list - ");
+					head = revert(head);
 					display(head);
 					break;
 			case 8:
-					printf("\n\n Your List - \n");
+					printf("\n\n Reversing your list - ");
+					reverse(head);
+					printf("\n\n Original list - ");
 					display(head);
-					printf(" NULL ");
 					break;
 			case 9:
+					printf("\n\n Your List - \n");
+					display(head);
+					break;
+			case 10:
 					return 0;
 			default:
 					printf("\n Please enter a valid option");
