52bf8281638d71479956ddcc0f4f580c2f96d6c2
Devashish Prasad <devashishkprasad@gmail.com>
2018-12-19 13:09:24 +0530

    Create DDAline.cpp

---
+++ SE/2nd sem/CG/DDAline.cpp
@@ -0,0 +1,75 @@
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+#include <iostream>
+
+void init()
+{
+	// making background color black as first  
+    // 3 arguments all are 0.0 
+    glClearColor(0.0, 0.0, 0.0, 1.0); 
+      
+    // making picture color white (in RGB mode),
+    glColor3f(1.0, 1.0, 1.0); 
+      
+    // breadth of picture boundary is 1 pixel 
+    glPointSize(3.0);  
+
+	// coordinate system
+	glMatrixMode(GL_PROJECTION);
+
+	// setting window dimension in X- and Y- direction 
+	gluOrtho2D(0.0, 1200.0, 0.0, 800.0);
+}
+
+void drawLine()
+{
+	init();
+
+	//DDA Function for line generation 
+	int X0=0, Y0=0, X1=200, Y1=200;
+    
+	// calculate dx & dy glMatrixMode(GL_PROJECTION);
+    int dx = X1 - X0; 
+    int dy = Y1 - Y0; 
+  
+    // calculate steps required for generating pixels 
+    int steps = dx > dy ? dx : dy; 
+  
+    // calculate increment in x & y for each steps 
+    float Xinc = dx / (float) steps; 
+    float Yinc = dy / (float) steps; 
+  
+    // Put pixel for each step 
+    float X = X0; 
+    float Y = Y0; 
+
+	glBegin(GL_POINTS);
+
+    for (int i = 0; i <= steps; i++) 
+    { 
+		printf("dd");
+        glVertex2i(X,Y);
+		X += Xinc;        // increment in x at each step 
+        Y += Yinc;        // increment in y at each step 
+        //delay(100);        
+    }
+
+  	glEnd(); 
+    glFlush(); 
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    glutInitWindowSize(1200, 800);
+	glutInitWindowPosition(0, 0);
+
+	int winid = glutCreateWindow("OpenGL - Creating a line");
+
+    glutDisplayFunc(drawLine);
+    glutMainLoop();
+ 
+   	return 0;
+}
