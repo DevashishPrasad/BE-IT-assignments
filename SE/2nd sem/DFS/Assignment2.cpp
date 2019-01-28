bc9be761ca48d37d6b16d43b5b8db026c2a62fd2
Devashish Prasad <devashishkprasad@gmail.com>
2019-01-28 16:02:35 +0530

    Update Assignment2.cpp

--- SE/2nd sem/DFS/Assignment2.cpp
+++ SE/2nd sem/DFS/Assignment2.cpp
@@ -8,101 +8,134 @@
 
 #include <iostream>
 #include "Queue.h"
+#include "Queue.cpp"
+#include <iomanip>
+#include <string.h>
+
 using namespace std;
 
 enum diseases {heartattack = 6, cancer = 5, fracture = 4, malaria = 3, chickenpox = 2,others = 1};
+
 struct Patient
 {
 	char disease[20];
-	char name[40];
+	char name[20];
 	int id;
 	char phone[10];
 };
 
 class Hospital
 {
-	Patient *patient;
+	Patient patient;
 
 	public:
 
-	void create();
-	void addPatient();
-	void displayAll();
-	void servePatient();
-	void deleteAll();
+	void getPatient();
+	void display();
+	int getPriority();
 };
 
-void Hospital::create()
-{
-	int total;
-	cout<<"\n\n Creating a new queue";
-	cout<<"\n Enter the maximum number of patients in queue - ";
-	cin>>total;
-	patient = new Patient[total];
-	cout<<"\n Queue created successfully";
-}
-
-void Hospital::addPatient()
+void Hospital::getPatient()
 {
 	cout<<"\n Enter the details for the patient - ";
-	cout<<"";
+	cout<<"\n Name - ";
+	cin>>patient.name;
+	cout<<"\n Disease - ";
+	cin>>patient.disease;
+	cout<<"\n ID - ";
+	cin>>patient.id;
+	cout<<"\n Phone - ";
+	cin>>patient.phone;
 }
 
-void Hospital::displayAll()
+void Hospital::display()
 {
-
+	cout<<setw(20)<<patient.name<<" ";
+	cout<<setw(20)<<patient.disease<<" ";
+	cout<<setw(5)<<patient.id<<" ";
+	cout<<setw(10)<<patient.phone<<endl;
 }
 
-void Hospital::servePatient()
+int Hospital::getPriority()
 {
+	diseases d;
 
-}
+	if(!strcmp(patient.disease,"heartattack") || !strcmp(patient.disease,"HeartAttack") || !strcmp(patient.disease,"Heartattack"))
+		d = heartattack;
+	else if(!strcmp(patient.disease,"cancer") || !strcmp(patient.disease,"Cancer"))
+		d = cancer;
+	else if(!strcmp(patient.disease,"fracture") || !strcmp(patient.disease,"Fracture"))
+		d = fracture;
+	else if(!strcmp(patient.disease,"malaria") || !strcmp(patient.disease,"Malaria"))
+		d = malaria;
+	else if(!strcmp(patient.disease,"chickenpox") || !strcmp(patient.disease,"Chickenpox")|| !strcmp(patient.disease,"ChickenPox"))
+		d = chickenpox;
+	else
+		d = others;
 
-void Hospital::deleteAll()
-{
-
+	return d;
 }
 
 int main() {
 
-	Hospital h;
+	Hospital h,*t;
 	Queue <Hospital> PriorityQueue;
 	int choice;
 
 	do
 	{
-		cout<<"\n\n\n----------- MENU -------------";
-		cout<<"\n 1. Create a Queue for patients";
-		cout<<"\n 2. Add patients in the queue";
-		cout<<"\n 3. Display all patients the queue";
-		cout<<"\n 4. Serve the patient in queue";
-		cout<<"\n 5. Delete the current queue";
-		cout<<"\n 6. Exit";
+		cout<<"\n\n\n=====================================";
+		cout<<"\n                MENU";
+		cout<<"\n=====================================";
+		cout<<"\n 1. Add a patient in the queue";
+		cout<<"\n 2. Display all patients the queue";
+		cout<<"\n 3. Serve next patient in queue";
+		cout<<"\n 4. Delete the current queue";
+		cout<<"\n 5. Exit";
+		cout<<"\n\n\n=====================================";
 		cout<<"\n Enter your choice - ";
 		cin>>choice;
 
 		switch(choice)
 		{
 			case 1:
-				h.create();
+				h.getPatient();
+				PriorityQueue.enqueue(h, h.getPriority());
+				cout<<"\n\n Patient added to the queue successfully ";
 				break;
 			case 2:
-				h.addPatient();
+				cout<<"\n\n Patients waiting in the Queue are - \n";
+				cout<<setw(20)<<"NAME ";
+				cout<<setw(20)<<"DISEASE ";
+				cout<<setw(5)<<"ID ";
+				cout<<setw(10)<<"PHONE "<<endl;
+				t = PriorityQueue.display();
+				for(int i=0; i<PriorityQueue.length() ;i++)
+					t[i].display();
 				break;
 			case 3:
-				h.displayAll();
+				if(!PriorityQueue.isempty())
+				{
+					cout<<"\n\n Following Patient was served successfully - \n";
+					cout<<setw(20)<<"NAME ";
+					cout<<setw(20)<<"DISEASE ";
+					cout<<setw(5)<<"ID ";
+					cout<<setw(10)<<"PHONE "<<endl;
+					PriorityQueue.dequeue().display();
+				}
+				else
+					cout<<"\n\n No patients in the queue";
 				break;
 			case 4:
-				h.servePatient();
+				while(!PriorityQueue.isempty())
+					PriorityQueue.dequeue();
+				cout<<"\n\n Queue was deleted successfully ";
 				break;
+
 			case 5:
-				h.deleteAll();
-				break;
-			case 6:
 				return 0;
 		}
 	}while(1);
 
 	return 0;
 }
-
