594ba04eeccbe3891179733f2998e05cabf75409
Devashish Prasad <devashishkprasad@gmail.com>
2019-01-31 16:01:16 +0530

    Create BinaryTree.cpp

---
+++ SE/2nd sem/DFS/BinaryTree.cpp
@@ -0,0 +1,18 @@
+/*
+ * BinaryTree.cpp
+ *
+ *  Created on: 31-Jan-2019
+ *      Author: h11
+ */
+
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BinaryTree::~BinaryTree() {
+	// TODO Auto-generated destructor stub
+}
+
