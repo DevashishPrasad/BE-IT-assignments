4743159cf85342fc98df3c365c877003380d23a8
Devashish Prasad <devashishkprasad@gmail.com>
2019-02-07 15:57:43 +0530

    Update BinaryTree.h

--- SE/2nd sem/DFS/BinaryTree.h
+++ SE/2nd sem/DFS/BinaryTree.h
@@ -8,27 +8,29 @@
 #ifndef BINARYTREE_H_
 #define BINARYTREE_H_
 
-template <class T>
-class BinaryTree {
-	struct Node{
+typedef struct Node{
 		struct Node *left;
 		struct Node *right;
-		T data;
-	};
-	struct Node *root;
+		int data;
+}Node;
+
+class BinaryTree {
+	int hl,hr;
+	Node *root;
 public:
+	Node* getroot();
 	BinaryTree();
 	virtual ~BinaryTree();
-	void insert(T d);
+	void insert(int d);
 	void display(int c=1);
-	int depth();
-	void displayleaves();
+	int height(Node* t);
+	void displayleaves(Node *t);
 	void inorder(Node *t);
 	void preorder(Node *t);
 	void postorder(Node *t);
 	void levelorder(Node *t);
+	Node* copy(Node *t);
 	int isempty();
-	void displayRoot();
 };
 
 #endif /* BINARYTREE_H_ */
