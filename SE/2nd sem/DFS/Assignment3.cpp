d48a3f670bb1f2fc0711a04a5b0e287d4690e964
Devashish Prasad <devashishkprasad@gmail.com>
2019-03-18 08:08:50 +0530

    assignment 6 not completed yet

--- SE/2nd sem/DFS/Assignment3.cpp
+++ SE/2nd sem/DFS/Assignment3.cpp
@@ -7,7 +7,7 @@
 //============================================================================
 
 #include "BinaryTree.h"
-//#include "BinaryTree.cpp"
+#include "BinaryTree.cpp"
 #include <iostream>
 
 using namespace std;
--- SE/2nd sem/DFS/Assignment6.cpp
+++ SE/2nd sem/DFS/Assignment6.cpp
@@ -46,6 +46,7 @@ class Vertex;
 class Graphedge
 {
 	int weight;
+	public:
 	Vertex* orignal;
 	Graphedge* nextlink;
 };
@@ -55,11 +56,12 @@ class Vertex
 	char* name;
 	Date dob;
 	int comments;
-	Vertex* downlink;
-	Graphedge* edgelink;
 	bool visited;
 
 	public:
+	Vertex* downlink;
+	Graphedge* edgelink;
+	
 	Vertex()
 	{
 		name = new char[20];
@@ -75,11 +77,13 @@ class Vertex
 		cin>>name;
 		cout<<"\n Date of Birth - ";
 		dob.getdate();
+		downlink = NULL;
 	}
 	void putdata()
 	{
 		cout<<"\n Name - "<<name;
-		cout<<"\n Date of Birth - "<<dob.putdate();
+		cout<<"\n Date of Birth - ";
+		dob.putdate();
 		cout<<"\n Comments - "<<comments;
 	}
 	void setvisited()
@@ -106,18 +110,38 @@ class Vertex
 class startnode
 {
 	int cnt;
-	Vertex* start;
 	public:
-	void creategraph()
+	Vertex* start;
+	void createNode()
 	{
+		cnt = 0;
 		cout<<"\n\n INFO: Making new User in the network";
 		Vertex *temp = start;
 
-		while(temp!=NULL)
-			temp=temp->downlink;
+		if(temp == NULL)
+		{
+			temp = new Vertex;
+			temp->makeuser();	
+			return;
+		}
 
-		temp = new Vertex;
+		while(temp->downlink!=NULL)
+		{
+			cnt++;
+			temp=temp->downlink;
+		}
+
+		temp->downlink = new Vertex;
 		temp->makeuser();
+	}
+	void initvisit()
+	{
+		Vertex *temp = start; 
+		while(temp != NULL)
+		{
+			temp->visitinit();
+			temp= temp->downlink;
+		}
 	}
 };
 
@@ -135,5 +159,54 @@ void traverse(Vertex* temp)
 }
 
 int main() {
+	startnode *graph = new startnode;
+	Vertex *startv = NULL;
+	graph->start = startv;
+	
+	int choice;
+	do
+	{
+		cout<<"\n\n\n ____________________________________";
+		cout<<"\n|                                    ";
+		cout<<"\n|                MENU                ";
+		cout<<"\n|____________________________________";
+		cout<<"\n|                                    ";
+		cout<<"\n|  1. Create New User   ";
+		cout<<"\n|  2. Make friends";
+		cout<<"\n|  3. Maximum friends    ";
+		cout<<"\n|  4. Maximum and minimum friends";
+		cout<<"\n|  5. Users having birthday this month";
+		cout<<"\n|  6. Traverse the graph";
+		cout<<"\n|  7. Exit                          ";
+		cout<<"\n|____________________________________";
+		cout<<"\n Enter your choice - ";
+		cin>>choice;
+
+		switch(choice)
+		{
+			case 1:
+				graph->createNode();
+				break;
+			case 2:
+				cout<<"\n Enter your name - ";
+				cin>>
+				cout<<"\n Enter the name of the person who you";
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				graph->initvisit();
+				traverse(startv); 
+				break;
+			case 7:
+				break;
+			default:
+				break;
+		}
+	}while(1);
 	return 0;
 }
