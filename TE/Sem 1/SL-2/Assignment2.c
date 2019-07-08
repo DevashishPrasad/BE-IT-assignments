de1bddeeedd0b3811e4063326a2a8677a932860f
Devashish Prasad <devashishkprasad@gmail.com>
2019-07-09 00:27:12 +0530

    Assign2 SL2

--- TE/Sem 1/SL-2/Assignment2.c
+++ TE/Sem 1/SL-2/Assignment2.c
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void print_array(int arr[100]){
-
+// Function to print the array on console
+void print_arr(int arr[100],int n){
+	int i;
+	
 	printf("\n Your array is - \n [ ");
  
 	for(i=0; i<n-1; i++)
@@ -16,11 +18,10 @@ void print_array(int arr[100]){
 	printf(" %d ]",arr[i]);
 }
 
-void QuickSort(int arr[100],int first,int last){
+// Quick sort
+void quick_sort(int arr[100],int first,int last){
 
-    int i,j,k,pivot,cmp=0,swap=0;
-
-    user temp;
+    int i,j,k,pivot,cmp=0,swap=0,temp;
 
     if(first<last)
     {
@@ -30,10 +31,10 @@ void QuickSort(int arr[100],int first,int last){
 
         while(i<j)
         {
-            while(strcmp(arr[i].name, arr[pivot].name) >= 0 && i<last)
+            while(arr[i] <= arr[pivot] && i<last)
                 i++;
 
-            while(strcmp(arr[j].name, arr[pivot].name) < 0)
+            while(arr[j] > arr[pivot])
                 j--;
 
             cmp++;
@@ -50,25 +51,90 @@ void QuickSort(int arr[100],int first,int last){
         arr[pivot]=arr[j];
         arr[j]=temp;
 
-        QuickSort(arr,first,j-1);
-        QuickSort(arr,j+1,last);
+        quick_sort(arr,first,j-1);
+        quick_sort(arr,j+1,last);
     }
-
-    p++;
-    printf("\n PASS %d - ", p);
-
-    print_arr(arr);
-
-    printf("\n Compariosns - %d ", cmp);
-    printf("\n Swaps - %d ", swap);
-    printf("\n Pivot - %s ", arr[pivot]);
 }
 
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void merge_sort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        merge_sort(arr, l, m); 
+        merge_sort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
 
 void main(){
 
 	// Declare the variables
-	int arr[100],n,i;
+	int arr[100],n,i,choice;
+	pid_t newpid,child;
 
 	// The main process starts
 	printf("\n This is the main process - ");
@@ -85,25 +151,53 @@ void main(){
 	}
 
 	// Print the array
-	print_arr(arr);
+	print_arr(arr,n);
 
-	printf("\n Forking the process - ");
+	printf("\n --------- MENU ---------");
+	printf("\n 1. Normal Execution ");
+	printf("\n 2. Zombie ");
+	printf("\n 3. Orphan ");
+	printf("\n Enter your choice - ");
+	scanf("%d",&choice);
+	
+	switch(choice){
+		case 1:
+			// Fork System call
+			printf("\n Forking the process - ");
+			newpid = fork();
 
-	pid_t newpid;
-	newpid = fork();
-
-	if(newpid == -1)
-		printf("\n Unfortunately the child was not born");
-	else if(newpid == 0){
-		printf("\n\n\n This is the child process -");
-		printf("\n Hello from the child process !");
-		printf("\n My id is -> %d",getpid());		
-		printf("\n\n [INFO] Child executed successfully \n\n\n	");
+			if(newpid == -1)
+				printf("\n Unfortunately the child was not born");
+			else if(newpid == 0){
+				// Inside child process
+				printf("\n\n\n Hello from the child process !");
+				printf("\n My id is -> %d",getpid());		
+				printf("\n Sorted Array using Quick sort -> ");
+				quick_sort(arr,0,n-1);
+				print_arr(arr,n);
+				printf("\n\n [INFO] Child executed successfully \n\n\n	");
+			}
+			else{
+				// Inside parent process
+				int status;
+				child = wait(&status);
+				if (child != -1)
+					printf("\n\n\n Waited for child with pid %d\n", child);
+				printf("\n Hello from the parent process !");
+				printf("\n My id is -> %d",getpid());
+				printf("\n Child Id -> %d",newpid);		
+				printf("\n Sorted Array using Merge sort -> ");
+				merge_sort(arr,0,n-1);
+				print_arr(arr,n);
+				printf("\n\n [INFO] Parent executed successfully \n\n\n");
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			printf("\n OOPS! INVALID INPUT");
 	}
-	else{
-		printf("\n Hello from the parent process !");
-		printf("\n My id is -> %d",getpid());
-		printf("\n Child Id -> %d",newpid);		
-		printf("\n\n [INFO] Parent executed successfully \n\n\n");
-	}
+
 }
--- TE/Sem 1/SL-2/Menu.sh
+++ TE/Sem 1/SL-2/Menu.sh
@@ -1,15 +1,47 @@
 !/bin/sh
 
 while test 1 -eq 1; do
-echo "MENU \n1) Add \n2) Subtract \n3) Divide \n4) Multiply \n5) Exit" 
+echo "MENU \n1) Add \n2) Subtract \n3) Multiply \n4) Divide \n5) Exit" 
 echo "\nEnter your choice - \c"
 read choice
 
 case "$choice" in
-1) echo "ADD";;
-2) echo "SUB";;
-3) echo "MUL";;
-4) echo "DIV";;
+1) 
+	echo "-------- ADDITION --------- "
+	echo "Enter number 1 -  \c"
+	read no1
+	echo "Enter number 2 -  \c"
+	read no2
+	res=$(($no1+$no2))
+	echo "Result of addition is - $res"
+;;
+2)
+	echo "-------- SUBTRACTION --------- "
+	echo "Enter number 1 -  \c"
+	read no1
+	echo "Enter number 2 -  \c"
+	read no2
+	res=$(($no1-$no2))
+	echo "Result of SUBTRACTION is - $res" 
+;;
+3)
+	echo "-------- MULTIPLICATION --------- "
+	echo "Enter number 1 -  \c"
+	read no1
+	echo "Enter number 2 -  \c"
+	read no2
+	res=$(($no1*$no2))
+	echo "Result of multiplication is - $res" 
+;;
+4) 
+	echo "-------- DIVISION --------- "
+	echo "Enter number 1 -  \c"
+	read no1
+	echo "Enter number 2 -  \c"
+	read no2
+	res=$(($no1/$no2))
+	echo "Result of DIVISION is - $res"
+;;
 5) exit;;
 *) echo "Invalid option";;
 esac
