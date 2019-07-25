93b6be0203304b8bdcf6e16271fe725412e9d5bb
Devashish Prasad <devashishkprasad@gmail.com>
2019-07-25 19:24:59 +0530

    SL3 going on

--- TE/Sem 1/SL-3/assignment2/script/js.js
+++ TE/Sem 1/SL-3/assignment2/script/js.js
@@ -5,15 +5,22 @@ var hid = document.getElementsByClassName("hidden")
 var sticky = nav.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-  	nav.classList.add("sticky")
-  	for(var x=0;x<hid.length;x++){
-	  	hid[x].style.top = "40px"
-	 }
-  } else {
-	nav.classList.remove("sticky")
-  	for(var x=0;x<hid.length;x++){
-		hid[x].style.top = "145px"
+	var offset = 145 - window.pageYOffset
+	console.log(offset)
+	if(offset > 40){
+		for(var x=0;x<hid.length;x++){
+		    hid[x].style.top = offset.toString() + "px"
+		}
 	}
-  }
+	else{
+		for(var x=0;x<hid.length;x++){
+		    hid[x].style.top = "40px"
+		}
+	}
+	if(window.pageYOffset >= sticky){
+		nav.classList.add("sticky")
+	}
+	else{
+		nav.classList.remove("sticky")
+	}
 }
