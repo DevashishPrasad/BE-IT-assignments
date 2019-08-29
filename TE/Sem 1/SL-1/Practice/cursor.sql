0a24ba622a9ba8e584c3fb46cac92aa5a09c27cf
Devashish Prasad <devashishkprasad@gmail.com>
2019-08-29 15:00:42 +0530

    Create cursor.sql

---
+++ TE/Sem 1/SL-1/Practice/cursor.sql
@@ -0,0 +1,19 @@
+create procedure run_cursor() 
+begin 
+DECLARE name_list varchar(2000) DEFAULT "";
+DECLARE finished INTEGER DEFAULT 0; 
+DECLARE name varchar(30) DEFAULT ""; 
+DEClARE show_names CURSOR FOR SELECT ename FROM Employee; 
+DECLARE CONTINUE HANDLER  FOR NOT FOUND SET finished = 1; 
+OPEN show_names; 
+get_name: LOOP 
+FETCH show_names INTO name; 
+IF finished = 1 THEN 
+LEAVE get_name; 
+END IF; 
+SELECT name;
+SET name_list = CONCAT(name,",",name_list);
+END LOOP get_name;
+SELECT name_list;
+CLOSE show_names;
+END$
