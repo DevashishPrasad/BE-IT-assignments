56d6a9040d0205bcb37e52d3655ba2b36ddbc1cd
Devashish Prasad <devashishkprasad@gmail.com>
2019-09-13 10:59:59 +0530

    Update employee.c

--- TE/Sem 1/SL-2/assignment 9/employee.c
+++ TE/Sem 1/SL-2/assignment 9/employee.c
@@ -13,71 +13,90 @@ struct employee{
 	char name[30]; 	
 	int id, salary;
 };
+
 // Main function
 int main()
 {
-	int choice;
-	int fd, sz;
+	int fd, sz, choice, cnt;
 	char *buf = (char *) calloc(100, sizeof(char)); 
+	char c;
 	struct employee emp;
+
+	printf("\n Using file -> thefile.txt with file descriptor number -> %d", fd); 
 		
-	printf("\n ============================================");
-	printf("\n |                   MENU                   |");
-	printf("\n ============================================");
-	printf("\n | 1. Add record                            |");		
-	printf("\n | 2. Remove record                         |");
-	printf("\n | 3. Update record                         |");	
-	printf("\n | 4. View records                          |");	
-	printf("\n ============================================");	
-	printf("\n Enter your choice - ");
-	scanf("%d",&choice);
+	while(1){
+		printf("\n ============================================");
+		printf("\n |                   MENU                   |");
+		printf("\n ============================================");
+		printf("\n | 1. Add record                            |");		
+		printf("\n | 2. Remove record                         |");
+		printf("\n | 3. Update record                         |");	
+		printf("\n | 4. View records                          |");	
+		printf("\n | 5. Exit                                  |");		
+		printf("\n ============================================");	
+		printf("\n Enter your choice - ");
+		scanf("%d",&choice);
 	
-	switch(choice){
-		case 1:
-			printf("\n Enter the name of the employee -> ");
-			scanf("%s",&emp.name);
-			printf("\n Enter the id of the employee -> ");
-			scanf("%s",&emp.id);			
-			printf("\n Enter the salary of the employee -> ");
-			scanf("%s",&emp.slaray);
-			sz = write(fd, "hello gais\n", strlen("hello gais\n"));
-			break;
-		case 2:
-			printf("\n Enter the id of the employee -> ");
-			scanf("%s",&emp.id);			
-			printf("\n Enter the salary of the employee -> ");
-			scanf("%s",&emp.slaray);			
-			break;			
-		default:
-			printf("\n Please Enter a valid choice");
+		switch(choice){
+			case 1:
+				// Take input from user
+				printf("\n Enter the name of the employee -> ");
+				scanf("%s",&emp.name);
+				printf("\n Enter the id of the employee -> ");
+				scanf("%d",&emp.id);			
+				printf("\n Enter the salary of the employee -> ");
+				scanf("%d",&emp.salary);
+			
+				// Open the file
+				fd = open("thefile.txt", O_WRONLY | O_APPEND);  
+			
+				// Insert the record
+				sz = write(fd, emp.name, strlen(emp.name));
+				sz = write(fd, "\t", strlen("\t"));
+				sprintf(buf, "%d", emp.id);
+				sz = write(fd, buf, strlen(buf));
+				sz = write(fd, "\t", strlen("\t"));				
+				sprintf(buf, "%d", emp.salary);			
+				sz = write(fd, buf, strlen(buf));	
+				sz = write(fd, "\n", strlen("\n"));				
+						
+				printf("\n Record was written successfully");
+				break;
+			case 2:
+				printf("\n Enter the id of the employee -> ");
+				scanf("%d",&emp.id);		
+				
+				fd = open("thefile.txt", O_RDONLY);
+				
+				cnt = 0;
+				
+				while(1){
+					c = read(fd,&c,1);
+					if(c == '\n')
+						cnt = 0;
+					if(c == '\t')
+						cnt++;
+					if(cnt == 1)
+						printf("%c",c);
+				}
+				
+				break;			
+			case 3:
+				printf("\n Enter the id of the employee -> ");
+				scanf("%d",&emp.id);			
+				break;
+			case 4:
+				// Open the file
+				fd = open("thefile.txt", O_RDONLY);
+				printf("\n Name\tid\tsalary\n");
+				while(read(fd,&c,1))
+					printf("%c",c);
+				break;
+			case 5:
+				exit(0);
+			default:
+				printf("\n Please Enter a valid choice");
+		}	
 	}	
-	// if file is not in directory  
-	// then file foo.txt is created. 
-	fd = open("foo.txt", O_RDWR | O_CREAT);  
-
-	printf("fd = %d\n", fd); 
-
-	if (fd <-1){ 
-		// print which type of error have in a code 
-		printf("Error Number % d\n", errno);  
-		// print program detail "Success or failure" 
-		perror("Program");                 
-		exit(1); 
-	} 
-	
-	sz = read(fd, c, 20);
-	 
-	printf("called read(%d, c, 10).  returned that"
-	 " %d bytes  were read.\n", fd, sz); 
-	c[sz] = '\0'; 
-	printf("Those bytes are as follows: %s\n", c); 
-
-	sz = write(fd, "hello gais\n", strlen("hello gais\n")); 
-	
-	if (close(fd) < 0){
-		perror("c1"); 
-		exit(1); 
-	}  
-	printf("closed the fd\n"); 
 	return 0; 
 }
